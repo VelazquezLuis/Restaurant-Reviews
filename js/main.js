90c8f887ad80e0d46c89be6f2a346dae1d4e15b9
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-10-03 20:20:11 -0700

    updated alt  description

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.name + " Image";
+  image.alt = "An Image Of " + restaurant.name ;
   li.append(image);
 
   const name = document.createElement('h1');
