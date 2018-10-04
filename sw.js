aa798a31945c7d620ba6d08361b557c3c1c5a9a0
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-10-04 11:20:26 -0700

    updated with ignoresearch on line 44

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@ self.addEventListener('install', function(event) {
 //fetch event
 self.addEventListener('fetch', function(event){
   event.respondWith(
-    caches.match(event.request).then(function(response){
+    caches.match(event.request, { ignoreSearch: true}).then(function(response){
       if(response) {
         console.log('Found', event.request, ' in cache');
         return response;
